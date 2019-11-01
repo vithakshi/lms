01a3a1cef104dbe8fabced8e6965f0dc3bc6ffb5
isurupoorna <isuru.poorna28@gmail.com>
2019-11-01 15:12:12 +0530

    second upload

--- app/src/main/java/com/nibm/nibm/MainActivity.java
+++ app/src/main/java/com/nibm/nibm/MainActivity.java
@@ -2,13 +2,31 @@ package com.nibm.nibm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.btn_login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent activityHome = new Intent(getApplicationContext(),home.class);
+                startActivity(activityHome);
+
+            }
+        });
+
     }
+
+
 }
